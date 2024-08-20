f355428ac8242e6899e3363feb138da78aae2906
Septicake <septicake056@gmail.com>
2024-08-20 19:29:51 -0400

    Corrected `polls` column name, set `channel` column to nullable, and renamed "pokemonPosition" to "offset"

--- src/main/kotlin/io/github/septicake/db/GuildEntity.kt
+++ src/main/kotlin/io/github/septicake/db/GuildEntity.kt
@@ -7,20 +7,20 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 object GuildTable : LongIdTable("serverinfo", columnName = "guildId") {
     val name = text("name")
-    val polls = long("polls")
-    val pokemonPosition = long("offset")
+    val polls = long("pollCount")
+    val offset = long("offset")
     val smashes = long("smash")
     val passes = long("pass")
-    val channel = long("channel")
+    val channel = long("channel").nullable()
 }
 
 class GuildEntity(id: EntityID<Long>) : LongEntity(id) {
     var name: String by GuildTable.name
     var polls: Long by GuildTable.polls
-    var pokemonPosition: Long by GuildTable.pokemonPosition
+    var offset: Long by GuildTable.offset
     var smashes: Long by GuildTable.smashes
     var passes: Long by GuildTable.passes
-    var channel: Long by GuildTable.channel
+    var channel: Long? by GuildTable.channel
 
     companion object : LongEntityClass<GuildEntity>(GuildTable)
 }
