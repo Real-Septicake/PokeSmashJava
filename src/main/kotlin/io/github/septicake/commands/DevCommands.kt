08c4ffec14445fc1a779f93a05f286d583f8c9a2
Septicake <septicake056@gmail.com>
2024-08-23 17:35:15 -0400

    Simplified dev "message" command

--- src/main/kotlin/io/github/septicake/commands/DevCommands.kt
+++ src/main/kotlin/io/github/septicake/commands/DevCommands.kt
@@ -1,6 +1,8 @@
 package io.github.septicake.commands
 
 import io.github.septicake.PokeSmashBot
+import io.github.septicake.cloud.annotations.ChannelRestriction
+import io.github.septicake.cloud.annotations.UserPermissions
 import io.github.septicake.db.GuildEntity
 import org.incendo.cloud.annotation.specifier.Greedy
 import org.incendo.cloud.annotations.Argument
@@ -14,7 +16,9 @@ class DevCommands(
 ) {
 
     @Command("message <server> <msg>")
-    @CommandDescription("Send a message to a server's channel. DEV COMMAND, CANNOT BE USED BY NORMAL USERS")
+    @CommandDescription("Send a message to a server's channel.")
+    @ChannelRestriction(devChannel = true)
+    @UserPermissions(botOwnerOnly = true)
     fun messageCommand(
         interaction: JDAInteraction,
         @Argument("server")
@@ -24,29 +28,26 @@ class DevCommands(
         str: String
     ) {
         val event = interaction.interactionEvent() ?: return
-        if(event.channel!!.id != bot.testingChannel) {
-            event.reply("Command cannot be used in this channel.").queue()
+        val info = transaction(bot.db) {
+            GuildEntity.findById(server)
+        }
+        if(info == null) {
+            event.reply("Server `$server` either does not exist or has not been populated.").queue()
         } else {
-            val info = transaction(bot.db) {
-                GuildEntity.findById(server)
-            }
-            if(info == null) {
-                event.reply("Server `$server` either does not exist or has not been populated.").queue()
-            } else {
-                if(info.channel != null){
-                    val channel = bot.jda.getTextChannelById(info.channel!!)
-                    if(channel == null) {
-                        event.reply("Server's channel does not exist.").queue()
-                        return
-                    } else {
-                        event.deferReply().queue()
-                        channel.sendMessage(str).queue()
-                        event.hook.sendMessage("Message sent.").queue()
-                    }
+            if(info.channel != null) {
+                val channel = bot.jda.getTextChannelById(info.channel!!)
+                if(channel == null) {
+                    event.reply("Server's channel does not exist.").queue()
+                    return
                 } else {
-                    event.reply("Server has not set a channel yet.")
+                    event.deferReply().queue()
+                    channel.sendMessage(str).queue()
+                    event.hook.sendMessage("Message sent.").queue()
                 }
+            } else {
+                event.reply("Server has not set a channel yet.")
             }
         }
+
     }
 }
