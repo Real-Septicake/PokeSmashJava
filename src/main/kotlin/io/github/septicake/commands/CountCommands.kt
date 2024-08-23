43109572aed58a247bfb5a3258bffa1d634873b1
Septicake <septicake056@gmail.com>
2024-08-23 17:36:12 -0400

    Added command for setting poll count, as well as updating command annotations

--- src/main/kotlin/io/github/septicake/commands/CountCommands.kt
+++ src/main/kotlin/io/github/septicake/commands/CountCommands.kt
@@ -1,42 +1,48 @@
 package io.github.septicake.commands
 
 import io.github.septicake.PokeSmashBot
+import io.github.septicake.cloud.annotations.GuildOnly
+import io.github.septicake.cloud.annotations.UserPermissions
 import io.github.septicake.db.GuildEntity
+import org.incendo.cloud.annotation.specifier.Range
 import org.incendo.cloud.annotations.Argument
 import org.incendo.cloud.annotations.Command
 import org.incendo.cloud.discord.jda5.JDAInteraction
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.slf4j.kotlin.getLogger
 
 class CountCommands(
     private val bot: PokeSmashBot,
 ) {
-    private val logger by getLogger()
 
-    @Command(value = "count [count]")
-    fun countCommand(
+    @GuildOnly
+    @UserPermissions(whitelistOnly = true)
+    @Command(value = "set count <count>")
+    fun countSetCommand(
         interaction: JDAInteraction,
         @Argument(value = "count", description = "new number of polls per call, can only be used by whitelisted users, value must be within 1 and 10")
-        count: Long?
+        @Range(min = "1", max = "10")
+        count: Int
     ) {
         val event = interaction.interactionEvent() ?: return
         val info = transaction(bot.db) {
             GuildEntity.findById(event.guild!!.idLong)
         }
         if(info != null){
-            if(count != null) {
-                val whitelisted = bot.userWhitelisted(event.guild!!.idLong, event.user.idLong)
-                if(whitelisted) {
-                    if(count in 1..10) {
-                        transaction(bot.db) { info.polls = count }
-                        event.reply("Count successfully set to `$count`").queue()
-                    } else {
-                        event.reply("Count must be within 1 and 10, value given was `$count`").queue()
-                    }
-                } else
-                    event.reply(bot.notWhitelistedResponse).queue()
-            } else
-                event.reply("Current poll count is `${info.polls}`").queue()
+            transaction(bot.db) { info.polls = count }
+            event.reply("Count successfully set to `$count`").queue()
         } else { event.reply("Server has not yet been populated").queue() }
+    }
+
+    @GuildOnly
+    @Command("count")
+    fun countCommand(
+        interaction: JDAInteraction
+    ) {
+        val event = interaction.interactionEvent() ?: return
+        val info = transaction(bot.db) {
+            GuildEntity.findById(event.guild!!.idLong)
+        }
+        if(info != null) event.reply("Current poll count is `${info.polls}`").queue()
+        else event.reply("Server has not yet been populated").queue()
     }
 }
