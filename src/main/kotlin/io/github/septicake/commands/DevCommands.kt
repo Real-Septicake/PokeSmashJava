da530cd00263873f68ad7f9fb12381e7c5c72b6e
Septicake <septicake056@gmail.com>
2024-08-24 20:31:41 -0400

    Added dev commands for announcements and bot shutdown

--- src/main/kotlin/io/github/septicake/commands/DevCommands.kt
+++ src/main/kotlin/io/github/septicake/commands/DevCommands.kt
@@ -4,6 +4,7 @@ import io.github.septicake.PokeSmashBot
 import io.github.septicake.cloud.annotations.ChannelRestriction
 import io.github.septicake.cloud.annotations.UserPermissions
 import io.github.septicake.db.GuildEntity
+import io.github.septicake.db.GuildTable
 import org.incendo.cloud.annotation.specifier.Greedy
 import org.incendo.cloud.annotations.Argument
 import org.incendo.cloud.annotations.Command
@@ -48,6 +49,64 @@ class DevCommands(
                 event.reply("Server has not set a channel yet.")
             }
         }
+    }
 
+    @Command("announce <msg>")
+    @CommandDescription("Announce a message to every server's channel.")
+    @UserPermissions(botOwnerOnly = true)
+    fun announceCommand(
+        interaction: JDAInteraction,
+        @Argument("msg")
+        @Greedy
+        msg: String
+    ) {
+        val event = interaction.interactionEvent() ?: return
+        event.deferReply().queue()
+        var sent = 0
+        var noChannel = 0
+        val format = msg.contains("%owner%")
+        transaction(bot.db) {
+            GuildTable.select(GuildTable.channel, GuildTable.id).forEach {
+                if(it[GuildTable.channel] == bot.testingChannel!!.toLong())
+                    return@forEach
+
+                if(it[GuildTable.channel] == null) {
+                    noChannel++
+                    return@forEach
+                }
+
+                if(format){
+                    val guild = bot.jda.getGuildById(it[GuildTable.id].value)!!
+                    val formattedStr = msg.replace("%owner%", "<@${guild.ownerId}>")
+                    bot.jda.getTextChannelById(it[GuildTable.channel]!!)!!.sendMessage(formattedStr).queue()
+                } else
+                    bot.jda.getTextChannelById(it[GuildTable.channel]!!)!!.sendMessage(msg).queue()
+
+                sent++
+            }
+        }
+        event.hook.sendMessage("Announcement sent to $sent server${if(sent > 0) "s" else ""}. $noChannel server${if(noChannel > 0) "s" else ""} did not have a channel.").queue()
+    }
+
+    @Command("shutdown")
+    @UserPermissions(botOwnerOnly = true)
+    suspend fun shutdownCommand(
+        interaction: JDAInteraction
+    ) {
+        val event = interaction.interactionEvent() ?: return
+        event.reply("Shutting down...").queue()
+        transaction(bot.db) {
+            GuildTable.select(GuildTable.channel, GuildTable.id).forEach {
+                if(it[GuildTable.channel] == bot.testingChannel!!.toLong())
+                    return@forEach
+
+                if(it[GuildTable.channel] == null) {
+                    return@forEach
+                }
+
+                bot.jda.getTextChannelById(it[GuildTable.channel]!!)!!.sendMessage("Bot shutting down...").queue()
+            }
+        }
+        bot.shutdown()
     }
 }
