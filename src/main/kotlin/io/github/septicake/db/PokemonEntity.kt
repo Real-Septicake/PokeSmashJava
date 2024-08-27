7e569a58f5f3a4d0678b2630df8bc6e0e002343d
Septicake <septicake056@gmail.com>
2024-08-27 11:31:45 -0400

    PokemonTable is now referenced by an Int id

--- src/main/kotlin/io/github/septicake/db/PokemonEntity.kt
+++ src/main/kotlin/io/github/septicake/db/PokemonEntity.kt
@@ -1,22 +1,22 @@
 package io.github.septicake.db
 
-import org.jetbrains.exposed.dao.LongEntity
-import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.dao.id.IntIdTable
 
-object PokemonTable : LongIdTable("pokemoninfo", columnName = "pokeId") {
+object PokemonTable : IntIdTable("pokemoninfo", columnName = "pokeId") {
     val smashWins = long("smashCount")
     val passWins = long("passCount")
     val smashes = long("smashVotes")
     val passes = long("passVotes")
 }
 
-class PokemonEntity(id: EntityID<Long>) : LongEntity(id) {
+class PokemonEntity(id: EntityID<Int>) : IntEntity(id) {
     var smashWins: Long by PokemonTable.smashWins
     var passWins: Long by PokemonTable.passWins
     var smashes: Long by PokemonTable.smashes
     var passes: Long by PokemonTable.passes
 
-    companion object : LongEntityClass<PokemonEntity>(PokemonTable)
+    companion object : IntEntityClass<PokemonEntity>(PokemonTable)
 }
