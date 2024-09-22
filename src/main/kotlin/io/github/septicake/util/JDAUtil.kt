5798535c3bbd7c4dc5ddad5088e9621cfde41525
solonovamax <solonovamax@12oclockpoint.com>
2024-09-22 16:05:47 -0400

    Fix send message shit
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/kotlin/io/github/septicake/util/JDAUtil.kt
+++ src/main/kotlin/io/github/septicake/util/JDAUtil.kt
@@ -1,18 +1,18 @@
 package io.github.septicake.util
 
+import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.messages.InlineMessage
 import dev.minn.jda.ktx.messages.Mentions
 import dev.minn.jda.ktx.messages.MessageCreate
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.WebhookClient
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.interactions.components.LayoutComponent
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction
-import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction
 import net.dv8tion.jda.api.utils.FileUpload
 import net.dv8tion.jda.api.utils.messages.MessageCreateData
 
-inline fun <T> WebhookClient<T>.sendMessage(
+suspend inline fun <T> WebhookClient<T>.sendMessage(
     content: String = "",
     embeds: Collection<MessageEmbed> = emptyList(),
     files: Collection<FileUpload> = emptyList(),
@@ -20,9 +20,9 @@ inline fun <T> WebhookClient<T>.sendMessage(
     tts: Boolean = false,
     mentions: Mentions = Mentions.default(),
     builder: InlineMessage<MessageCreateData>.() -> Unit = {},
-): WebhookMessageCreateAction<T> = sendMessage(MessageCreate(content, embeds, files, components, tts, mentions, builder))
+): T = sendMessage(MessageCreate(content, embeds, files, components, tts, mentions, builder)).await()
 
-inline fun MessageChannel.sendMessage(
+suspend inline fun MessageChannel.sendMessage(
     content: String = "",
     embeds: Collection<MessageEmbed> = emptyList(),
     files: Collection<FileUpload> = emptyList(),
@@ -30,4 +30,4 @@ inline fun MessageChannel.sendMessage(
     tts: Boolean = false,
     mentions: Mentions = Mentions.default(),
     builder: InlineMessage<MessageCreateData>.() -> Unit = {},
-): MessageCreateAction = sendMessage(MessageCreate(content, embeds, files, components, tts, mentions, builder))
+): Message? = sendMessage(MessageCreate(content, embeds, files, components, tts, mentions, builder)).await()
